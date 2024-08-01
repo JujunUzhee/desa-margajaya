33f69f758ba4e5bdb480c9a5e5104252dc0ca0b6
JujunUzhee <103125165+JujunUzhee@users.noreply.github.com>
2024-08-01 17:43:17 +0700

    meta

--- src/pages/Beranda.jsx
+++ src/pages/Beranda.jsx
@@ -1,4 +1,3 @@
-
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import BeritaComponents from '../components/BeritaComponents';
@@ -21,7 +20,7 @@ const Beranda = () => {
         <div className="bg-base-100 dark:bg-gray-900">
             <Header />
             <div className="container mx-auto py-4 px-4">
-                <div className="flex flex-col md:flex-row items-center bg-white rounded-3xl p-3 md:p-5 shadow-xl">
+                <div className="flex flex-col md:flex-row items-center bg-white dark:bg-white-bg-dark rounded-3xl p-3 md:p-5 shadow-xl">
                     <motion.div
                         className="w-full md:w-1/2 flex justify-center"
                         initial="hidden"
@@ -36,7 +35,7 @@ const Beranda = () => {
                         animate="visible"
                         variants={textVariants}
                     >
-                        <h1 className='text-2xl md:text-3xl font-bold text-gray-900 dark:text-gray-100 md:mb-4  mt-3 mb-2 md:ml-4'>
+                        <h1 className='text-2xl md:text-3xl font-bold text-gray-900 dark:text-gray-100 md:mb-4 mt-3 mb-2 md:ml-4'>
                             Selamat Datang Di Desa Margajaya
                         </h1>
                         <p className="text-gray-700 dark:text-gray-300 mb-2 md:ml-4">
@@ -53,7 +52,6 @@ const Beranda = () => {
                             className="btn btn-success mt-3 text-slate-50 md:ml-4 hover:text-green-500 hover:bg-white hover:border hover:border-green-500">
                             Sejarah Desa
                         </Link>
-
                     </motion.div>
                 </div>
             </div>
@@ -64,13 +62,14 @@ const Beranda = () => {
                     <div className="bg-transparent border-0 p-2">
                         <div className="flex flex-col md:flex-row items-center">
                             <div className="flex justify-center items-center md:w-1/3 mb-4 md:mb-0">
-                                <img src="/static/img/logo_majalengka.jpeg" className="w-2/4  h-auto mx-auto rounded-lg" alt="Logo Bangkalan" />
+                                <img src="/static/img/logo_majalengka.jpeg" className="w-2/4 h-auto mx-auto rounded-lg" alt="Logo Bangkalan" />
                             </div>
                             <div className="md:w-2/3">
                                 <div className="p-4 text-left">
                                     <p className="text-gray-700 dark:text-gray-300 mb-4">
-                                        Website resmi Desa Margajaya, Kec. Lemah Sugih, Kab. Majalengka, Jawa Barat 45465. Sebagai media komunikasi dan transparansi pemerintah, situs ini bertujuan untuk menyediakan informasi penting, layanan, dan pengumuman kepada seluruh masyarakat Desa Margajaya. Di sini, Anda dapat menemukan berita terkini, program pemerintah, dan berbagai layanan administrasi yang diperlukan untuk memudahkan interaksi antara warga dan pemerintah desa. Kami berkomitmen untuk memberikan informasi yang akurat dan terkini demi kemajuan dan kesejahteraan masyarakat.        </p>
-                                    <Link to="/visi-misi" className='btn btn-success mt-3 text-slate-50  hover:text-green-500 hover:bg-white hover:border hover:border-green-500'>Visi & Misi </Link>
+                                        Website resmi Desa Margajaya, Kec. Lemah Sugih, Kab. Majalengka, Jawa Barat 45465. Sebagai media komunikasi dan transparansi pemerintah, situs ini bertujuan untuk menyediakan informasi penting, layanan, dan pengumuman kepada seluruh masyarakat Desa Margajaya. Di sini, Anda dapat menemukan berita terkini, program pemerintah, dan berbagai layanan administrasi yang diperlukan untuk memudahkan interaksi antara warga dan pemerintah desa. Kami berkomitmen untuk memberikan informasi yang akurat dan terkini demi kemajuan dan kesejahteraan masyarakat.
+                                    </p>
+                                    <Link to="/visi-misi" className='btn btn-success mt-3 text-slate-50 hover:text-green-500 hover:bg-white hover:border hover:border-green-500'>Visi & Misi </Link>
                                 </div>
                             </div>
                         </div>
